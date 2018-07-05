09b972a1933853aa7c2ea567dbff7db2e7d20300
Eugene Arshinov <earshinov@gmail.com>
2018-07-05 13:06:31 +0300

    Initial demo (working)

--- demo-app/src/app/app.module.ts
+++ demo-app/src/app/app.module.ts
@@ -1,17 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Component, NgModule, forwardRef } from '@angular/core';
+import { DemoLibModule, PolymorphicComponent } from 'demo-lib';;
 
 
-import { AppComponent } from './app.component';
+@Component({
+  selector: 'app-p',
+  template: '<p><ng-content></ng-content></p>',
+  providers: [
+    {provide: PolymorphicComponent, useExisting: forwardRef(() => ParagraphComponent)}
+  ]
+})
+export class ParagraphComponent extends PolymorphicComponent {
+  identify(): string {
+    return "ParagraphComponent";
+  }
+}
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+}
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [ParagraphComponent, AppComponent],
+  imports: [BrowserModule, DemoLibModule],
   providers: [],
   bootstrap: [AppComponent]
 })
